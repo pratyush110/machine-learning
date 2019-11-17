ec3f77e58f4d8113a2aa55d855cbf33f419dd4b0
pratyush110 <pratyush.priyadarshi11@gmail.com>
2019-11-17 22:46:57 +0530

    update

--- classification/iris.py
+++ classification/iris.py
@@ -1,4 +1,4 @@
-from sklearn import datasets
+ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 #from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
